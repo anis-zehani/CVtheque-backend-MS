19b5a875a3e5325297b037e22fce6cc6ce87cff4
GitLabOdix <azaheni@smartgraphe.com>
2019-12-19 16:43:25 +0100

    Mise en place des sources

--- src/main/java/com/odix/fr/model/Utilisateur.java
+++ src/main/java/com/odix/fr/model/Utilisateur.java
@@ -1,15 +1,18 @@
 package com.odix.fr.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
@@ -31,16 +34,36 @@ public class Utilisateur implements Serializable {
 	//Attention : ne doit pas être Generated car il doit avoir le même UUID que le MS Maître
 	@Column(name = "id", updatable = false, nullable = false, unique=true)
 	private UUID id;
-
+	
 	@Column(unique = true)
 	private String identite;
 	
+    @Column
+	private String telephone;
+	
     @Column(unique = true)
 	private String email;
-
+	
+	@Column
+	private String posteOccupe;
+	
+	@Column(length = 4096)
+	private String descriptionDetaillee;
+	
+	@Column(unique = true)
+	private String username;
+	
+	@Column
+	private String password;
+	
 	@Column(length = 1024)
 	private String urlPhoto;
+	
+	@Column
+	private LocalDateTime dateAjout;
 
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	Entreprise entreprise;
 
 	public Utilisateur() {
 		super();
@@ -50,16 +73,16 @@ public class Utilisateur implements Serializable {
 		return identite;
 	}
 
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
 	public void setIdentite(String identite) {
 		this.identite = identite;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
 	}
 
 	public String getEmail() {
@@ -69,6 +92,42 @@ public class Utilisateur implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public String getPosteOccupe() {
+		return posteOccupe;
+	}
+
+	public void setPosteOccupe(String posteOccupe) {
+		this.posteOccupe = posteOccupe;
+	}
+
+	public String getDescriptionDetaillee() {
+		return descriptionDetaillee;
+	}
+
+	public void setDescriptionDetaillee(String descriptionDetaillee) {
+		this.descriptionDetaillee = descriptionDetaillee;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UUID getId() {
+		return id;
+	}
 	
 	public String getUrlPhoto() {
 		return urlPhoto;
@@ -76,5 +135,21 @@ public class Utilisateur implements Serializable {
 
 	public void setUrlPhoto(String urlPhoto) {
 		this.urlPhoto = urlPhoto;
+	}
+
+	public LocalDateTime getDateAjout() {
+		return dateAjout;
+	}
+
+	public void setDateAjout(LocalDateTime dateAjout) {
+		this.dateAjout = dateAjout;
+	}
+
+	public Entreprise getEntreprise() {
+		return entreprise;
+	}
+
+	public void setEntreprise(Entreprise entreprise) {
+		this.entreprise = entreprise;
 	}
 }
