7256698e5482d21b1e9a309a15cc585b39298915
odix <odix@odix-laptop>
2020-04-21 21:32:30 +0100

    tests

---
+++ src/test/java/com/odix/fr/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.odix.fr;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
