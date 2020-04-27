c8ba052f1967f5099d9d9278bcdbd8fdbf5d89d5
odix <odix@odix-laptop>
2020-04-27 07:00:39 +0100

    Suppression CrossOrigin

--- src/main/java/com/odix/fr/controller/EntrepriseController.java
+++ src/main/java/com/odix/fr/controller/EntrepriseController.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.odix.fr.model.Entreprise;
 import com.odix.fr.service.EntrepriseService;
 
-@CrossOrigin
+
 @RestController
 @RequestMapping("/api/entreprise")
 public class EntrepriseController {
